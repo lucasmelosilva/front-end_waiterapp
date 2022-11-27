a6aca9abed1f20111c81513ffb3d6912a283a8d5
lucas melo <lucas.melo044@gmail.com>
2022-11-27 13:53:05 -0300

    add Orders component

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -1,5 +1,64 @@
-import { Container } from "./styles";
-
+import { Container, Board, OrdersContainer } from "./styles";
 export function Orders() {
-  return <div />;
+  return (
+    <Container>
+      <Board>
+        <header>
+          <span>🕑</span>
+          <strong>Fila de espera</strong>
+          <span>(1)</span>
+        </header>
+
+        <OrdersContainer>
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 items</span>
+          </button>
+
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 items</span>
+          </button>
+        </OrdersContainer>
+      </Board>
+      <Board>
+        <header>
+          <span>🕑</span>
+          <strong>Fila de espera</strong>
+          <span>(1)</span>
+        </header>
+
+        <OrdersContainer>
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 items</span>
+          </button>
+
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 items</span>
+          </button>
+        </OrdersContainer>
+      </Board>
+      <Board>
+        <header>
+          <span>🕑</span>
+          <strong>Fila de espera</strong>
+          <span>(1)</span>
+        </header>
+
+        <OrdersContainer>
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 items</span>
+          </button>
+
+          <button type="button">
+            <strong>Mesa 2</strong>
+            <span>2 items</span>
+          </button>
+        </OrdersContainer>
+      </Board>
+    </Container>
+  );
 }
